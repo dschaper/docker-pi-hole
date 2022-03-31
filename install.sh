62231c0cd85d197abd77a4034ea6f5c54b3832d8
Dan Schaper <dan.schaper@pi-hole.net>
2022-03-31 14:00:45 -0700

    nother
    Signed-off-by: Dan Schaper <dan.schaper@pi-hole.net>

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ export PIHOLE_SKIP_OS_CHECK=true
 # Run the installer in unattended mode using the preseeded variables above and --reconfigure so that local repos are not updated
 # curl -sSL https://install.pi-hole.net | bash -sex -- --unattended
 echo "curling tarball"
-curl -sL https://api.github.com/repos/dschaper/pi-hole/tarball/fix/touch_guard | tar -xz --strip-components 1 -C /etc/.pihole
+curl -sL https://api.github.com/repos/dschaper/pi-hole/tarball/fix/touch_guard | tar -xz --strip-components 1
 bash -ex automated\ install/basic-install.sh --unattended
 
 # At this stage, if we are building a :nightly tag, then switch the Pi-hole install to dev versions
